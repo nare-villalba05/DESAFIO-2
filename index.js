e660627a08ce176ebf2ace0ece6bedb37c5ed937
Nare <narela00528@gmail.com>
2024-04-16 17:56:32 -0300

    Nuevo comit

---
+++ index.js
@@ -0,0 +1,11 @@
+let prod1="Mouse"
+let precioU=7000
+let cantidad
+
+alert('Hola estas por comprar un producto que es:'+ prod1 + ' y su precio es de: '+ precioU )
+
+let cantidadDesada =prompt('Ingrese la cantidad que quiere comprar: ')
+
+const costoTotal= precioU * cantidadDesada
+
+alert('Usted comprò'+ cantidadDesada + "" +prod1+"."+"El costo total es: $"+ costoTotal)
